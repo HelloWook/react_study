73377f33e294683db9bb661127e67d8c48f17696
남정욱 <113816822+HelloWook@users.noreply.github.com>
2023-05-02 23:36:10 +0900

    4-2
    4-2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter, Routes, Route, NavLink,  } from 'react-router-dom';
+function Home ()
+{
+  return <div>
+    <h2>HOME</h2>
+    home...
+  </div>
+}
+function Topics ()
+{
+  return <div>
+    <h2>Topics</h2>
+    Topics...
+  </div>
+}
+function Contact ()
+{
+  return <div>
+    <h2>Contact</h2>
+    Contact...
+  </div>
+}
 
+function App()
+{
+  return <div>
+  <h1>Hello React Router Dom </h1>
+  <ul>
+  <li><NavLink to ='/'>Home</NavLink></li>
+  <li><NavLink to ='/topics'>Topics</NavLink></li>
+  <li><NavLink to ='/contact'>Contact</NavLink></li>
+  </ul>
+  <Routes>
+  <Route path="/" element={<Home />}></Route>
+  <Route path="/topics" element={<Topics />}></Route>
+  <Route path="/contact" element={<Contact />}></Route>
+  </Routes>
+  </div>
+}
 ReactDOM.render(
   <React.StrictMode>
+     <HashRouter> 
     <App />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
